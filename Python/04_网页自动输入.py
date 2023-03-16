d04306e834ef11ac49da4984a29ed1f6ff69cfe8
Zaker <253830084@qq.com>
2023-03-16 23:19:21 +0800

    Update 04_网页自动输入.py

--- Python/04_网页自动输入.py
+++ Python/04_网页自动输入.py
@@ -2,15 +2,15 @@
 
 from selenium import webdriver
 
-browser = webdriver.Chrome('/Users/zhaoyan/Downloads/chromedriver')
+browser = webdriver.Chrome('/Users/xx/Downloads/chromedriver')
 
 browser.get('http://prod.ci.yit.com/job/yit-prod-resolve-job/build?delay=0sec')
 
 
 
 /* 登陆Jenkins */
-browser.find_element_by_id('j_username').send_keys('zhaoyan')
-browser.find_element_by_name('j_password').send_keys('SEuGaOQV4u578')
+browser.find_element_by_id('j_username').send_keys('xx')
+browser.find_element_by_name('j_password').send_keys('xxxx')
 browser.find_element_by_id('yui-gen1').click
 
 
@@ -22,12 +22,12 @@ browser.find_element_by_link_text('Build with Parameters').click
 
 from selenium import webdriver
 
-browser = webdriver.Chrome('/Users/zhaoyan/Downloads/chromedriver')
+browser = webdriver.Chrome('/Users/xx/Downloads/chromedriver')
 
 browser.get('http://prod.ci.yit.com/job/yit-prod-resolve-job/build?delay=0sec')
 
-browser.find_element_by_id('j_username').send_keys('zhaoyan')
-browser.find_element_by_name('j_password').send_keys('SEuGaOQV4u578')
+browser.find_element_by_id('j_username').send_keys('xx')
+browser.find_element_by_name('j_password').send_keys('xxx')
 browser.find_element_by_id('yui-gen1-button').click()
 
 
