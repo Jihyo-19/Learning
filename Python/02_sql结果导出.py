06a59b471a5f46943241940df17cb55460750838
Zaker <253830084@qq.com>
2023-03-16 23:20:03 +0800

    Update 02_sql结果导出.py

--- Python/02_sql结果导出.py
+++ Python/02_sql结果导出.py
@@ -10,27 +10,8 @@ level = 1
 start_time = '2018-05-31 22:00:00'
 end_time = '2018-06-01 16:00:00'
 
-start_sql = '''select present.id from (
-	select * from yitiao_present_gift_record a where a.created_at >= '{0}' and a.created_at <= '{1}' and lucky_bag_id != 0
-) present inner join (
-	select * from yitiao_receive_gift_record a where a.created_at >= '{0}' and a.created_at <= '{1}' and `status` = 1
-) receive on present.customer_id = receive.customer_id inner join (
-	select * from yitiao_order_user_lucky_bag where bag_type = "VIP"
-) bag on present.lucky_bag_id = bag.id
-group by present.id;'''.format(start_time, end_time)
 
 print(start_sql)
-
-
-gift_sql_format = '''select id from (
-	select present.id, sum(if(present.created_at > receive.created_at, 1, 0)) ps from (
-		select * from yitiao_present_gift_record a1 where a1.created_at >= '{0}' and a1.created_at <= '{1}' and lucky_bag_id != 0 and a1.id not in ({2})
-	) present left join (
-		select * from yitiao_receive_gift_record a2 where a2.created_at >= '{0}' and a2.created_at <= '{1}' and `status` = 1 and a2.present_record_id not in ({2})
-	) receive on present.customer_id = receive.customer_id
-	group by present.id
-) tmp where ps = 0'''
-
 
 def run_sql(sql):
     conn = MySQLdb.connect(host='rr-bp1x8k8pea67a270co.mysql.rds.aliyuncs.com', user='yit_prod_r',
